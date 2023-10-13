36f2192ddb9c99b6e6fe584ddae97cdd2cb27794
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-14 10:17:12 +1300

    Improve the tests and check coverage

--- game.test.js
+++ game.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import {
   bowlingScore,
   scoreFrame,
@@ -8,240 +8,244 @@ import {
   isDoubleStrike,
 } from './game'
 
-test('ScoreFrame scores a normal frame', () => {
-  const frame = [1, 3]
+describe('scoreFrame() correctly calculates', () => {
+  it('a normal frame', () => {
+    const frame = [1, 3]
 
-  const actual = scoreFrame(frame)
+    const actual = scoreFrame(frame)
 
-  expect(actual).toBe(4)
+    expect(actual).toBe(4)
+  })
+
+  it('a spare frame', () => {
+    const frame1 = [6, 4]
+    const frame2 = [4, 4]
+
+    const actual = scoreFrame(frame1, frame2)
+
+    expect(actual).toBe(14) // 10 + next roll
+  })
+
+  it('a strike frame', () => {
+    const frame1 = [10, 0]
+    const frame2 = [4, 4]
+
+    const actual = scoreFrame(frame1, frame2)
+
+    expect(actual).toBe(18) // 10 + next 2 rolls
+  })
+
+  it('a double strike frame', () => {
+    const frame1 = [10, 0]
+    const frame2 = [10, 0]
+    const frame3 = [5, 4]
+
+    const actual = scoreFrame(frame1, frame2, frame3)
+
+    expect(actual).toBe(25) // 10 + next 2 rolls
+  })
 })
 
-test('ScoreFrame scores a spare frame', () => {
-  const frame1 = [6, 4]
-  const frame2 = [4, 4]
+describe('sumFrame() correctly handles', () => {
+  it('a 2 ball frame', () => {
+    const frame = [1, 2]
 
-  const actual = scoreFrame(frame1, frame2)
+    const actual = sumFrame(frame)
 
-  expect(actual).toBe(14) // 10 + next roll
+    expect(actual).toBe(3)
+  })
+
+  it('a 3 ball frame', () => {
+    const frame = [10, 2, 3]
+
+    const actual = sumFrame(frame)
+
+    expect(actual).toBe(15)
+  })
 })
 
-test('ScoreFrame scores a strike frame', () => {
-  const frame1 = [10, 0]
-  const frame2 = [4, 4]
+describe('boolean functions return correct value', () => {
+  it.each([
+    { frame1: [10, 0], frame2: [10, 0], expected: true },
+    { frame1: [10, 0], frame2: [5, 5], expected: false },
+  ])(
+    'isDoubleStrike($frame1, $frame2) -> $expected',
+    ({ frame1, frame2, expected }) => {
+      const actual = isDoubleStrike(frame1, frame2)
 
-  const actual = scoreFrame(frame1, frame2)
+      expect(actual).toBe(expected)
+    }
+  )
 
-  expect(actual).toBe(18) // 10 + next 2 rolls
+  it.each([
+    { frame1: [10, 0], expected: true },
+    { frame1: [5, 5], expected: false },
+  ])('isStrike($frame1) -> $expected', ({ frame1, expected }) => {
+    const actual = isStrike(frame1)
+
+    expect(actual).toBe(expected)
+  })
+
+  it.each([
+    { frame1: [5, 5], expected: true },
+    { frame1: [5, 3], expected: false },
+  ])('isSpare($frame1) -> $expected', ({ frame1, expected }) => {
+    const actual = isSpare(frame1)
+
+    expect(actual).toBe(expected)
+  })
 })
 
-test('ScoreFrame scores a double strike frame', () => {
-  const frame1 = [10, 0]
-  const frame2 = [10, 0]
-  const frame3 = [5, 4]
+describe('bowlingScore() correctly scores', () => {
+  it('a simple game', () => {
+    const frames = [
+      [2, 0],
+      [4, 2],
+      [6, 0],
+      [2, 4],
+      [1, 5],
+      [7, 0],
+      [5, 2],
+      [7, 0],
+      [2, 6],
+      [8, 1],
+    ]
 
-  const actual = scoreFrame(frame1, frame2, frame3)
+    const actual = bowlingScore(frames)
 
-  expect(actual).toBe(25) // 10 + next 2 rolls
-})
+    expect(actual).toBe(64)
+  })
 
-test('sumFrame handles a 2 ball frame', () => {
-  const frame = [1, 2]
-  const actual = sumFrame(frame)
-  expect(actual).toBe(3)
-})
+  it('a game with spares', () => {
+    const frames = [
+      [6, 1],
+      [4, 0],
+      [6, 4],
+      [2, 7],
+      [3, 5],
+      [5, 0],
+      [5, 5],
+      [0, 0],
+      [1, 6],
+      [7, 2],
+    ]
 
-test('sumFrame handles a 3 ball frame', () => {
-  const frame = [10, 2, 3]
-  const actual = sumFrame(frame)
-  expect(actual).toBe(15)
-})
+    const actual = bowlingScore(frames)
 
-test('isDoubleStrike - true case', () => {
-  const frame1 = [10, 0]
-  const frame2 = [10, 0]
-  const actual = isDoubleStrike(frame1, frame2)
-  expect(actual).toBe(true)
-})
+    expect(actual).toBe(71)
+  })
 
-test('isDoubleStrike - false case', () => {
-  const frame1 = [10, 0]
-  const frame2 = [5, 5]
-  const actual = isDoubleStrike(frame1, frame2)
-  expect(actual).toBe(false)
-})
+  it('a game with spares and strikes', () => {
+    const frames = [
+      [6, 4],
+      [8, 0],
+      [10, 0],
+      [2, 7],
+      [5, 5],
+      [4, 0],
+      [10, 0],
+      [2, 1],
+      [2, 6],
+      [4, 4],
+    ]
 
-test('isStrike - true case', () => {
-  const frame = [10, 0]
-  const actual = isStrike(frame)
-  expect(actual).toBe(true)
-})
+    const actual = bowlingScore(frames)
 
-test('isStrike - false case', () => {
-  const frame = [5, 5]
-  const actual = isStrike(frame)
-  expect(actual).toBe(false)
-})
+    expect(actual).toBe(104)
+  })
 
-test('isSpare - true case', () => {
-  const frame = [5, 5]
-  const actual = isSpare(frame)
-  expect(actual).toBe(true)
-})
+  it('a game with spares, strikes and a double strike', () => {
+    const frames = [
+      [1, 2],
+      [6, 4],
+      [5, 4],
+      [10, 0],
+      [7, 2],
+      [10, 0],
+      [10, 0],
+      [5, 2],
+      [7, 0],
+      [4, 4],
+    ]
 
-test('isSpare - false case', () => {
-  const frame = [3, 6]
-  const actual = isSpare(frame)
-  expect(actual).toBe(false)
-})
+    const actual = bowlingScore(frames)
 
-test('Score 64 (simple game)', () => {
-  const frames = [
-    [2, 0],
-    [4, 2],
-    [6, 0],
-    [2, 4],
-    [1, 5],
-    [7, 0],
-    [5, 2],
-    [7, 0],
-    [2, 6],
-    [8, 1],
-  ]
+    expect(actual).toBe(119)
+  })
 
-  const actual = bowlingScore(frames)
+  it('a game with a strike on the last frame', () => {
+    const frames = [
+      [1, 2],
+      [6, 4],
+      [5, 4],
+      [10, 0],
+      [7, 2],
+      [10, 0],
+      [10, 0],
+      [5, 2],
+      [7, 0],
+      [10, 10, 10],
+    ]
 
-  expect(actual).toBe(64)
-})
+    const actual = bowlingScore(frames)
 
-test('Score 71 (with spares)', () => {
-  const frames = [
-    [6, 1],
-    [4, 0],
-    [6, 4],
-    [2, 7],
-    [3, 5],
-    [5, 0],
-    [5, 5],
-    [0, 0],
-    [1, 6],
-    [7, 2],
-  ]
+    expect(actual).toBe(141)
+  })
 
-  const actual = bowlingScore(frames)
+  it('a perfect game', () => {
+    const frames = [
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 10, 10],
+    ]
 
-  expect(actual).toBe(71)
-})
+    const actual = bowlingScore(frames)
 
-test('Score 104 (with spares and strikes):', () => {
-  const frames = [
-    [6, 4],
-    [8, 0],
-    [10, 0],
-    [2, 7],
-    [5, 5],
-    [4, 0],
-    [10, 0],
-    [2, 1],
-    [2, 6],
-    [4, 4],
-  ]
+    expect(actual).toBe(300)
+  })
 
-  const actual = bowlingScore(frames)
+  it('a game with bonus roll with double strike', () => {
+    const frames = [
+      [10, 0], // Strike
+      [7, 3], // Spare
+      [4, 2],
+      [10, 0], // Strike
+      [2, 8], // Spare
+      [10, 0], // Strike
+      [10, 0], // Strike (Double)
+      [9, 1], // Spare
+      [3, 6],
+      [10, 10, 7], // Strike (Double) with bonus roll
+    ]
 
-  expect(actual).toBe(104)
-})
+    const actual = bowlingScore(frames)
 
-test('Score 119 (with spares, strikes and a double strike):', () => {
-  const frames = [
-    [1, 2],
-    [6, 4],
-    [5, 4],
-    [10, 0],
-    [7, 2],
-    [10, 0],
-    [10, 0],
-    [5, 2],
-    [7, 0],
-    [4, 4],
-  ]
+    expect(actual).toBe(178)
+  })
 
-  const actual = bowlingScore(frames)
+  it('an almost perfect game with only one final strike', () => {
+    const frames = [
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 3, 7],
+    ]
 
-  expect(actual).toBe(119)
-})
+    const actual = bowlingScore(frames)
 
-test('Score 141 (includes a strike on the last frame):', () => {
-  const frames = [
-    [1, 2],
-    [6, 4],
-    [5, 4],
-    [10, 0],
-    [7, 2],
-    [10, 0],
-    [10, 0],
-    [5, 2],
-    [7, 0],
-    [10, 10, 10],
-  ]
-
-  const actual = bowlingScore(frames)
-
-  expect(actual).toBe(141)
-})
-
-test('Score 300 (perfect game)', () => {
-  const frames = [
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 10, 10],
-  ]
-
-  const actual = bowlingScore(frames)
-
-  expect(actual).toBe(300)
-})
-
-test('A game with bonus but not perfect game (Score 178)', () => {
-  const frames = [
-    [10, 0], // Strike
-    [7, 3], // Spare
-    [4, 2],
-    [10, 0], // Strike
-    [2, 8], // Spare
-    [10, 0], // Strike
-    [10, 0], // Strike (Double)
-    [9, 1], // Spare
-    [3, 6],
-    [10, 10, 7], // Strike (Double) with bonus roll
-  ]
-
-  const actual = bowlingScore(frames)
-
-  expect(actual).toBe(178)
-})
-
-test('Score 283', () => {
-  const frames = [
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 0],
-    [10, 3, 7],
-  ]
-
-  const actual = bowlingScore(frames)
-
-  expect(actual).toBe(283)
+    expect(actual).toBe(283)
+  })
 })
